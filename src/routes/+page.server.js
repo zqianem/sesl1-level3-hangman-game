50809f33eeb467b5f9648007c1a84e202c5c9c00
Em Zhan <zqianem@gmail.com>
2023-04-26 06:02:57 -0500

    Implement new game creation on homepage

---
+++ src/routes/+page.server.js
@@ -0,0 +1,17 @@
+import { error as sk_error, redirect } from '@sveltejs/kit';
+import random_word from 'random-word';
+
+export const actions = {
+  default: async ({ locals: { supabase } }) => {
+    const word = random_word();
+    const { data, error } = await supabase
+      .from('hangman_games')
+      .insert({ word })
+      .select()
+      .maybeSingle();
+
+    if (error) throw sk_error(500, error);
+
+    throw redirect(303, data.id);
+  },
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<form method="POST">
+  <button type="submit">New game</button>
+</form>
