6067b4fbebaaef2f8cbaf5a4a46cdc7c4cf5397f
Em Zhan <zqianem@gmail.com>
2023-04-27 18:14:02 -0500

    Make letter/char comparison case-insensitive

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,9 @@
+const collator = new Intl.Collator('en', { sensitivity: 'base' });
+
 export function letter_in_word(letter, word) {
   return [...word].some((char) => letter_matches_char(letter, char));
 }
 
 export function letter_matches_char(letter, char) {
-  return letter === char || letter === char.normalize('NFD')[0];
+  return collator.compare(letter, char) === 0;
 }
