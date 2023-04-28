bc038a5b962d37632f4bee1de283629b18ce512d
k890m <94244306+k890m@users.noreply.github.com>
2023-04-28 17:34:13 -0400

    Update +page.svelte

--- src/routes/custom/+page.svelte
+++ src/routes/custom/+page.svelte
@@ -1,2 +1,31 @@
 <script>
 </script>
+
+<script>
+
+</script>
+
+<h1>Create A Custom Game</h1>
+
+<div class="create">
+  <form>
+    <label for="word">Enter a word:</label>
+    <input type="text" id="word" name="word"/><br /><br
+    />
+    <label for="code">Enter a code:</label>
+    <input type="text" id="code" name="code"/><br /><br
+    />
+    
+    <input type="submit" value="Submit" /><br /><br />
+  </form>
+</div>
+
+<h1>Play Custom Game</h1>
+
+<div class="createdGame">
+  <form>
+    <label for="word">Enter game code:</label>
+    <input type="text" id="code" name="code" /><br /><br />
+    <input type="submit" value="Submit" />
+  </form>
+</div>
