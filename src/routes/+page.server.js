b5b3a1fd26aae5ba9287bbdea470ced9fea60b32
Em Zhan <zqianem@gmail.com>
2023-04-28 02:07:22 -0500

    Avoid new games for players with unfinished ones

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,6 +3,29 @@ import { random_word } from '$lib/server';
 
 export async function load({ cookies, locals: { supabase } }) {
   const player = cookies.get('playerid');
+  const game =
+    (await get_unfinished_game(supabase, player)) ??
+    (await create_new_game(supabase, player));
+
+  throw redirect(303, game);
+}
+
+async function get_unfinished_game(supabase, player) {
+  const { data, error } = await supabase
+    .from('hangman_games')
+    .select()
+    .eq('player', player)
+    .is('win', null)
+    .order('created_at', { ascending: false })
+    .limit(1)
+    .maybeSingle();
+
+  if (error) throw sk_error(500, error);
+
+  return data?.id;
+}
+
+async function create_new_game(supabase, player) {
   const word = random_word();
   const { data, error } = await supabase
     .from('hangman_games')
@@ -12,5 +35,5 @@ export async function load({ cookies, locals: { supabase } }) {
 
   if (error) throw sk_error(500, error);
 
-  throw redirect(303, data.id);
+  return data.id;
 }
