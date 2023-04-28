2b7bc81be1ac2010414ab5fd1d7c64f1aa588428
Em Zhan <zqianem@gmail.com>
2023-04-27 23:31:53 -0500

    Automatically redirect homepage to a new game

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,18 +1,16 @@
 import { error as sk_error, redirect } from '@sveltejs/kit';
 import { random_word } from '$lib/server';
 
-export const actions = {
-  default: async ({ cookies, locals: { supabase } }) => {
-    const player = cookies.get('playerid');
-    const word = random_word();
-    const { data, error } = await supabase
-      .from('hangman_games')
-      .insert({ word, player })
-      .select()
-      .maybeSingle();
+export async function load({ cookies, locals: { supabase } }) {
+  const player = cookies.get('playerid');
+  const word = random_word();
+  const { data, error } = await supabase
+    .from('hangman_games')
+    .insert({ word, player })
+    .select()
+    .maybeSingle();
 
-    if (error) throw sk_error(500, error);
+  if (error) throw sk_error(500, error);
 
-    throw redirect(303, data.id);
-  },
-};
+  throw redirect(303, data.id);
+}
